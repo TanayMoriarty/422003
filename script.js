fb9ca4614c21713594004a2f4107426561ace48c
Bhargav1231 <130851063+Bhargav1231@users.noreply.github.com>
2024-06-17 14:21:59 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-alert ("Welcome to our website")
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form');
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        alert('Message sent!');
-    });
-});
